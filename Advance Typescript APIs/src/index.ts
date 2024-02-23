e6ca6b6a9953ff769c0d481c81c4db5f15f959c8
Prem <prempravinsapkale@gmail.com>
2024-02-23 11:00:00 +0530

    Partial

--- Advance Typescript APIs/src/index.ts
+++ Advance Typescript APIs/src/index.ts
@@ -6,11 +6,20 @@ interface User {
    password: string;
 }
 
-type UpdateProps = Pick<User, 'name' & 'age' & 'email'>;
+type UpdateProps = Pick<User, 'name' | 'age' | 'email'>;
+
+type UpdatePropsOptiona = Partial<UpdateProps>;
 
 function updateUser(updatedProps: UpdateProps) {
    // hit the database to update the user
    console.log(updatedProps);
 }
 
+function updateUser2(updatedProps: UpdatePropsOptiona) {
+   // hit the database to update the user
+   console.log(updatedProps);
+}
+
 updateUser({ name: 'harkirat', age: 19, email: 'harkirat@gmail.com' });
+
+updateUser2({ name: 'harkirat' }); // don't complain
